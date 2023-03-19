c9a5361ed72e440d1c9c5686a223fc7eaaef2993
karol_orlowicz <karol.orlowicz@gmail.com>
2023-03-19 02:03:55 +0100

    check context load failed test

--- wallet-service/src/test/java/pl/kargolek/walletservice/WalletServiceApplicationTests.java
+++ wallet-service/src/test/java/pl/kargolek/walletservice/WalletServiceApplicationTests.java
@@ -3,9 +3,7 @@ package pl.kargolek.walletservice;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.reactive.function.client.WebClient;
 import pl.kargolek.walletservice.client.CryptoPriceServiceClient;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -16,13 +14,9 @@ class WalletServiceApplicationTests {
 
 	@Autowired
 	private CryptoPriceServiceClient cryptoPriceServiceClient;
-	@Autowired
-	@Qualifier("etherscanWebClient")
-	private WebClient etherscanWebClient;
 
 	@Test
 	void contextLoads() {
-		assertThat(etherscanWebClient).isNotNull();
 		assertThat(cryptoPriceServiceClient).isNotNull();
 	}
 }
